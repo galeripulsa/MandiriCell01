eed8306fbf37ef4760d88b489a1bbf291f2a019d
galeripulsa <agusbaba1987@gmail.com>
2025-06-11 12:26:31 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>Selamat datang di Mandiri Cell</h1>
+      <p>Website ini masih dalam tahap pengembangan.</p>
+    </div>
+  );
+}
+
+export default App;
